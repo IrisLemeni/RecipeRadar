c3ff6dcea3948338607c88d59dda3312684f2dd7
Iris <lemeniriis@yahoo.ro>
2024-04-11 22:55:04 +0300

    munca de ieri...

---
+++ main.js
@@ -0,0 +1,56 @@
+const searchForm = document.querySelector("form");
+const searchResultDiv = document.querySelector(".search-result");
+const container = document.querySelector(".container");
+let searchQuery = "";
+const APP_ID = "55732f31";
+const APP_key = "c2eccf832a8f6c6d314348b24d277f02";
+// console.log(container)
+searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  searchQuery = e.target.querySelector("input").value;
+  fetchAPI();
+});
+
+async function fetchAPI() {
+  const baseURL = `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${APP_key}&from=0&to=20`;
+  const response = await fetch(baseURL);
+  const data = await response.json();
+  generateHTML(data.hits);
+  console.log(data);
+}
+
+function generateHTML(results) {
+  container.classList.remove("initial");
+  let generatedHTML = "";
+  results.map((result) => {
+    generatedHTML += `
+      <div class="item">
+        <img src="${result.recipe.image}" alt="img">
+        <div class="flex-container">
+          <h1 class="title">${result.recipe.label}</h1>
+          <a class="view-btn" target="_blank" href="${
+            result.recipe.url
+          }">View Recipe</a>
+        </div>
+        <p class="item-data">Calories: ${result.recipe.calories.toFixed(2)}</p>
+      </div>
+    `;
+  });
+  searchResultDiv.innerHTML = generatedHTML;
+}
+
+var slideIndex = 0;
+   showSlides();
+
+   function showSlides() {
+      var i;
+      var slides = document.getElementsByClassName("mySlides");
+      for (i = 0; i < slides.length; i++) {
+         slides[i].style.display = "none";
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {slideIndex = 1}
+      slides[slideIndex-1].style.display = "block";
+      setTimeout(showSlides, 6000);
+   }
+
